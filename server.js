fb86665f0c12b0e7e0e2d4db54da4c935000eb4e
Shea Schwenn <sheaschwenn@gmail.com>
2023-04-25 16:02:59 -0700

    landing page and note page now connected and rendered on client side

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ const express = require('express');
 const path = require('path');
 const notes = require('./db/db.json')
 
-const PORT = 3001
+const PORT = 3001;
 
-const app = express 
+const app = express() ;
+
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,"index.html"))
+})
+
+app.get('/notes',(req,res)=>{
+    res.sendFile(path.join(__dirname,"/public/notes.html" ))
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Visit http://localhost:${PORT}`);
+})
