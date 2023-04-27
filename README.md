cf8e79876e9690724b703dcee5a4cdecdcab0fb2
Shea Schwenn <sheaschwenn@gmail.com>
2023-04-26 18:49:12 -0700

    route for deletion is now working

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
 # Note-Taker
-Application to help save notes
+Note taker application
+## Technology Used 
+
+| Technology Used         | Resource URL           | 
+| ------------- |:-------------:| 
+| HTML    | [https://developer.mozilla.org/en-US/docs/Web/HTML](https://developer.mozilla.org/en-US/docs/Web/HTML) | 
+| CSS     | [https://developer.mozilla.org/en-US/docs/Web/CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)      |   
+| Git | [https://git-scm.com/](https://git-scm.com/)     |    
+| JavaScript | [https://developer.mozilla.org/en-US/docs/Web/JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)     |   
+| Node.JS | [https://nodejs.org/en/docs](https://nodejs.org/en/docs)     | 
+| Express.js | [https://expressjs.com/](https://expressjs.com/)     |   
+| Uuid | [https://www.npmjs.com/package/uuid](https://www.npmjs.com/package/uuid)     |     
+
+  
+
+
+## Description 
+
+[Visit the Deployed Site](https://youtu.be/BFyeuLhjcPY)
+
+An application that takes user inputs as notes that persist until deleted.  Old notes can be clicked on and seen again and new notes can be written at any time by choosing the plus symbol in the upper right hand corner. 
+
+
+
+![Site Langing Page](./site.gif)
+
+
+## Table of Contents 
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Learning Points](#learning-points)
+* [Author Info](#author-info)
+* [Credits](#credits)
+* [License](#license)
+
+
+
+## Installation
+npm i to install all dependencies needed 
+## Usage 
+Can be used to take notes
+
+
+![note taking page](./public/assets/images/Screenshot%202023-04-26%20075041.png)
+
+
+
+## Learning Points 
+
+This application is my first application deployed on Heroku which is exciting. It is also my first time creating my own local server and training it how to take get and post responses.  
+*  Express.js
+* Backend get and post requests
+* Client-side Post reuqests
+* Static files with express.js
+* More with fs and JSON files
+
+
+## Author Info
+
+### Shea Schwennicke 
+
+
+* [Portfolio](https://sheaschwenn.github.io/Portfolio/)
+* [LinkedIn](https://www.linkedin.com/in/shea-schwennicke-76a378210/)
+* [Github](https://github.com/sheaschwenn)
+
+
+
+
+## License
+
+MIT License 
+
+For more information please refer to LICENSE in the repo
+
+---
+
+© 2023 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
--- server.js
+++ server.js
@@ -2,74 +2,104 @@ const express = require('express');
 const fs = require('fs')
 const path = require('path');
 const notes = require('./db/db.json')
-const {v4:uuidv4} = require("uuid")
+const { v4: uuidv4 } = require("uuid")
 
 
 const PORT = 3001;
 
-const app = express() ;
+const app = express();
 
 app.use(express.json())
 app.use(express.static('public'));
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,"index.html"))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"))
 })
 
-app.get('/notes',(req,res)=>{
-    res.sendFile(path.join(__dirname,"/public/notes.html" ))
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
 })
 
-app.get('/api/notes',(req,res)=>{
+app.get('/api/notes', (req, res) => {
     console.log(`${req.method} request recived /api/notes`);
-    fs.readFile('db/db.json',"utf8", (err,data)=>{
-        if(err){
+    fs.readFile('db/db.json', "utf8", (err, data) => {
+        if (err) {
             console.log(err)
         }
-        else{
+        else {
             // making sure that the data is in the right form before sending it back to the client
-    res.json(JSON.parse(data))
+            res.json(JSON.parse(data))
         }
-})
+    })
 })
 
-app.post('/api/notes',(req,res)=>{
+app.post('/api/notes', (req, res) => {
     console.log(`${req.method} request recived /api/notes`);
-    
-    const{title,text}=req.body
 
-    if(title && text){
-        const newNote ={
-            id : uuidv4(),
+    const { title, text } = req.body
+
+    if (title && text) {
+        const newNote = {
+            id: uuidv4(),
             title,
             text,
         };
-        fs.readFile('db/db.json',"utf8", (err,data)=>{
-            if(err){
+        fs.readFile('db/db.json', "utf8", (err, data) => {
+            if (err) {
                 console.log(err)
             }
-            else{
+            else {
                 const parsedNotes = JSON.parse(data);
 
                 parsedNotes.push(newNote)
 
-                fs.writeFile('db/db.json',JSON.stringify(parsedNotes,null,4),(Err)=>
-                Err ?console.error(Err):console.log('new note saved'))
+                fs.writeFile('db/db.json', JSON.stringify(parsedNotes, null, 4), (Err) =>
+                    Err ? console.error(Err) : console.log('new note saved'))
             }
         });
-        const response ={
-            status:"sucess",
-            body:newNote
+        const response = {
+            status: "sucess",
+            body: newNote
         }
         console.log(response)
         res.status(201).json(response);
     }
-    else{
+    else {
         res.status(500).json("Error in saving note")
     }
 })
 
+app.delete('/api/notes/:id', (req, res) => {
+    const deletedNote = req.params.id
+    fs.readFile('db/db.json', "utf8", (err, data) => {
+        if (err) {
+            console.log(err)
+            res.status(500).json("Error deleting note")
+        }
+        else {
+            const parsedNotes = JSON.parse(data).filter(note => {
+                return note.id != deletedNote
+            })
 
-app.listen(PORT,()=>{
-    console.log(`Visit http://localhost:${PORT}`);
+            // for (let i = 0; i < parsedNotes.length; i++) {
+            //     if (deletedNote === parsedNotes[i].id) {
+            //         parsedNotes.splice(parsedNotes[i],1)
+
+                    fs.writeFile('db/db.json', JSON.stringify(parsedNotes, null, 4), (Err) =>
+                        Err ? console.error(Err) : console.log('note deleted'))
+            //             return 
+            //     }
+
+            // }
+            res.send(`${req.method} request called`)
+        }
+
+
+        
+
+    })
 })
+
+    app.listen(PORT, () => {
+        console.log(`Visit http://localhost:${PORT}`);
+    })
